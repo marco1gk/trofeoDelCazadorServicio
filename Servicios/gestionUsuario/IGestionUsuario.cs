4e42b3c531bdab6cc8908504e0ef1d627532ec53
Marco Antonio Varillas Olguin <zS22013628@estudiantes.uv.mx>
2024-10-16 00:15:11 -0600

    empiezo trabajo callbacks

---
+++ Servicios/gestionUsuario/IGestionUsuario.cs
@@ -0,0 +1,51 @@
+﻿using System.ServiceModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using dataAccess;
+
+namespace trofeoDelCazadorServicio.Servicios.gestionUsuario
+{
+    [ServiceContract(CallbackContract = typeof(IGestionUsuario))]
+    internal interface IGestionUsuario 
+    {
+        [OperationContract(IsOneWay = true)]
+        void agregarCuenta(Cuenta cuenta);
+    }
+
+    interface IGestionUsuarioCallBack
+    {
+        [OperationContract]
+        void UsersResponse(String response);
+    }
+
+    [DataContract]
+    public class Cuenta
+    {
+        [DataMember]
+        public int idCuenta { get; set; }
+
+        [DataMember]
+        public string correo { get; set; }
+
+        [DataMember]
+        public string contrasenia { get; set; }
+
+        [DataMember]
+        public string nombre { get; set; }
+
+        [DataMember]
+        public string apellido { get; set; }
+
+        [DataMember]
+        public DateTime fechaRegistro { get; set; }   
+        }
+}
+
+
+
+
+
