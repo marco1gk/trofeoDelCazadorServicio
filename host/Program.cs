2db5dd30677e619765718db9819958cdf2aca4be
marco1gk <zS22013628@estudiantes.uv.mx>
2024-10-18 12:06:53 -0600

    Add files via upload

---
+++ host/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using trofeoDelCazadorServicio;
+using trofeoDelCazadorServicio.Servicios.gestionUsuario;
+
+
+namespace host
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(gestionUsuarioServicio)))
+            {
+                host.Open();
+                Console.WriteLine("Servicio machin");
+                Console.ReadLine();
+
+            }  
+        }
+    }
+}
